439cfc639fab5944b8c0874c7b2e8b1877aebaeb
arslanhakim <arslanhaakim@gmail.com>
2024-07-31 03:56:32 +0500

    cors

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   optionsSuccessStatus: 200,
