f0c53d309155dc41e842315e10ae67bb2f648a0e
arslanhakim <arslanhaakim@gmail.com>
2024-07-31 02:50:48 +0500

    cors updated

--- server.js
+++ server.js
@@ -17,10 +17,20 @@ const port = process.env.PORT || 5000;
 app.use(express.static("public"));
 
 app.use(bodyParser.json());
+const allowedOrigins = ["http://localhost:5173", "https://allamalquraan.com"];
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-frontend-domain.com", "*"],
-    methods: ["GET", "POST", "PUT", "DELETE", "UPDATE"],
+    function(origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ["GET", "POST"],
   })
 );
 
